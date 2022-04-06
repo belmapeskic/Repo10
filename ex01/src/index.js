904a1d3bb062cd459d53b0335a329bae2261adca
belmapeskic <belma.peskic@gmail.com>
2022-04-07 00:15:37 +0200

    Adding file index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "cat",
+    name: "Lovely",
+    legs: 4,
+    friends: ["Lulu", "Fufu"]
+}
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
